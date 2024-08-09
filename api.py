1896f2daa08af6cdfd9752d60e40c1bee248079c
Augusto <65096407+augustomtt@users.noreply.github.com>
2024-08-09 12:33:01 -0300

    render host ip change 0.0.0.0

--- api.py
+++ api.py
@@ -307,4 +307,4 @@ def nuevosdatos_return():
 print(f"Server running on {host}:{port}")
 os.makedirs('Results', exist_ok=True)
 if __name__ == '__main__':
-    app.run(host="127.0.0.1",port=7777,debug=True)
+    app.run(host="0.0.0.0",port=7777,debug=True)
